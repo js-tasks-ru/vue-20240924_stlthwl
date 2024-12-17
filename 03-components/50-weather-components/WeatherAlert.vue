41624c9db20b5050e52f7733bef51f099869a037
stlthwl <stlthwl92@mail.ru>
2024-12-17 15:35:24 +0300

    fix: добавлены SFC для 50-weather-components

---
+++ 03-components/50-weather-components/WeatherAlert.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="weather-alert">
+    <span class="weather-alert__icon">⚠️</span>
+    <span class="weather-alert__description">{{ alert.sender_name }}:<br>{{ alert.description }}</span>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import './WeatherApp.css'
+
+export default defineComponent({
+  name: 'WeatherAlert',
+
+  props: {
+    alert: Object,
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
